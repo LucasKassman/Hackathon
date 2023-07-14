850ff02393f17e8d95c05df5f3d89c95abf4fff9
Todd <toddkassman@gmail.com>
2023-07-13 22:21:47 -0700

    example of how to select the latest measurements from tiDB

--- GraphTest/Average.py
+++ GraphTest/Average.py
@@ -1,19 +1,26 @@
+import datetime
 from connector import *
 def getAveragePing(servernum,cursor):
     #with get_connection() as connection:
         #with connection.cursor() as cursor:
 
-            query = "SELECT ping_latency_ns FROM ping_data WHERE server_hostname = %s"
+            now = datetime.datetime.now()
+            starttime = now - datetime.timedelta(days=1, hours=12, minutes=1)
+            #print(starttime)
+
+            #query = "SELECT ping_latency_ns FROM ping_data WHERE server_hostname = %s"
+            query = "SELECT * from test.ping_data A where server_hostname='"+servernum+"' and A.ping_time >= '"+starttime.strftime("%Y-%m:%d %H:%M:%S")+"'"
+            #print(query)
             #query = "SELECT ping_latency_ns, server_hostname FROM ping_data"
             #desired_value = 'desired_value'
-            cursor.execute(query, (servernum,))
+            cursor.execute(query)
             results = cursor.fetchall()
             #print(results)
 
             total = 0
             count = 0
             for row in results:
-                value = row[0]
+                value = row[1]
                 total += value
                 count += 1
 
