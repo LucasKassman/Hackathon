59e077eb4616d46c58f06573730a6c52a6c22d1f
Todd <toddkassman@gmail.com>
2023-07-19 13:22:57 -0700

    Update MainWindow to show the full server and selected servers side-by-side

--- GraphTest/MainWindow.py
+++ GraphTest/MainWindow.py
@@ -1,25 +1,64 @@
 # Import Library
-import tkinter
+import tkinter as tk
 from tkinter import ttk
 from graphtest3 import *
 from evaluator import *
 
-#import tkinter as tk
-#from tkinter import *
+class LB_Items(tk.Frame):
+    def __init__(self, root, title, values, bSelectAll, side=tk.LEFT):
+            super().__init__(root)
+            self.lblabel = tk.Label(self,
+			    text = title,
+		    #	font = ("Times New Roman", 10),
+			    padx = 10, pady = 10)
+            self.lblabel.pack(side=tk.TOP)
+        
+            # for scrolling vertically
+            self.yscrollbar = tk.Scrollbar(self)
+            self.yscrollbar.pack(side = tk.RIGHT, fill = tk.Y)
+            activeStyle = "dotbox" # dotbox, non, underline...
+            sm = "extended" # single, browse, multiple, or extended
+            self.list = tk.Listbox(self, selectmode = sm, activestyle=activeStyle,
+			    yscrollcommand = self.yscrollbar.set)
+            # Widget expands horizontally and
+            # vertically by assigning both to
+            # fill option
+            self.list.pack(padx = 10, pady = 10,
+	             expand = tk.YES, fill = "both", side=tk.LEFT)
 
+            for item in values:
+	       	    self.list.insert(tk.END, item)
+                
+            # Attach listbox to vertical scrollbar
+            self.yscrollbar.config(command = self.list.yview)
+
+            if bSelectAll:
+             #start with everthing selected
+                self.list.select_set(0, tk.END)
+            
+            self.pack(fill="both", expand=True, side=side)
+
+    #helper menthod
+    def get(self,i):
+        return self.list.get(i)
+    #helper menthod
+    def getAll(self):
+        return self.list.get(0,tk.END)
+ 
 #
 # main window that is the launch point for options and creating graph sub-windows.
 #  This is a sub-class of tk.Tk, which is often refered to as 'root' in various examples.
 class MainWindow(tk.Tk):
-    
+
     # constructor
     def __init__(self, title="MainWindow()"):
         super().__init__()
         print("in MainWindow  __init__")
         self.title(title)
-        self.geometry("400x800")
+        self.geometry("600x400")
         self.subWindows=[] # a list of all the child windows we have
         self.fullServerList = getServers()
+        #ttk delete this:  self.grid(row=2, column=4)
  
     # create a listbox
     def CreateListbox(self,title, values, bSelectAll):
@@ -31,7 +70,7 @@ class MainWindow(tk.Tk):
         
         # for scrolling vertically
         yscrollbar = tk.Scrollbar(self)
-        yscrollbar.pack(side = tk.RIGHT, fill = tk.Y)
+        yscrollbar.pack(side = tk.LEFT, fill = tk.Y)
         activeStyle = "dotbox" # dotbox, non, underline...
         sm = "extended" # single, browse, multiple, or extended
         list = tk.Listbox(self, selectmode = sm, activestyle=activeStyle,
@@ -40,7 +79,7 @@ class MainWindow(tk.Tk):
         # vertically by assigning both to
         # fill option
         list.pack(padx = 10, pady = 10,
-	        expand = tk.YES, fill = "both")
+	        expand = tk.YES, fill = "both", side=tk.LEFT)
 
         for item in values:
 	       	list.insert(tk.END, item)
@@ -55,7 +94,7 @@ class MainWindow(tk.Tk):
         return list
 
     def selectedServers(self):
-        return self.selected_display.get(0,tk.END)
+        return self.selected_display.getAll()
             
     # Add the option buttons to this window.  Could be modified to add more complex widgets
     def AddButtons(self):
@@ -63,16 +102,16 @@ class MainWindow(tk.Tk):
         tk.Button(self, text="Show", command=self.show).pack(pady=10) # just a test case for now
         tk.Button(self, text="Hide", command=self.hide).pack(pady=10) # just a test case for now
 
-        self.server_selector   = self.CreateListbox("Select from these Servers", self.fullServerList, True)
-        self.selected_display  = self.CreateListbox("Selected Servers", self.fullServerList, False)
-        
+        self.server_selector   = LB_Items(self,"Select from these Servers", self.fullServerList, True)
+        self.selected_display  = LB_Items(self, "Selected Servers", self.fullServerList, False, tk.RIGHT)
+
         def on_select(evt):
             if (evt.widget.curselection()):
-                self.selected_display.delete(0, tk.END)
-                for i in self.server_selector.curselection():
-                    self.selected_display.insert(tk.END, self.server_selector.get(i))
+                self.selected_display.list.delete(0, tk.END)
+                for i in self.server_selector.list.curselection():
+                    self.selected_display.list.insert(tk.END, self.server_selector.list.get(i))
 
-        self.server_selector.bind('<<ListboxSelect>>', on_select)
+        self.server_selector.list.bind('<<ListboxSelect>>', on_select)
 
 
     # launch the child window (i.e. graph)
@@ -115,7 +154,7 @@ class MainWindow(tk.Tk):
 #
 # Window containing the plot data
 # This is a subclass of Toplevel which allows us to create indepent windows.
-class ChildWindow(tkinter.Toplevel):
+class ChildWindow(tk.Toplevel):
     def __init__(self, sl):
         super().__init__()
         print("in ChildWindow __init__")
